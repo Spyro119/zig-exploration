02b1269029d7ca87bf8e9f21a0b5088c68de9c1d
Samuel Jubinville-Baril <samuel.jubinville@outlook.com>
2024-07-13 22:10:01 -0400

    added a comment

--- src/main.zig
+++ src/main.zig
@@ -5,26 +5,29 @@ const ProgramError = error{
 };
 
 pub fn main() !void {
-    var file_path: []u8 = undefined;
+    const allocator = std.heap.page_allocator;
+    const buffer = try allocator.alloc(u8, 1024 * 3); // 3kb seems to be the sweet spot
+    defer allocator.free(buffer);
 
     if (std.os.argv.len < 2) {
         std.log.err("Incorrect number of arguments: wanted 2, got {d}", .{std.os.argv.len});
         return ProgramError.WrongAmountOfArguments;
     }
-    file_path = std.mem.sliceTo(std.os.argv[1], 1);
 
+    const file_path: []u8 = std.mem.sliceTo(std.os.argv[1], 1);
     const file = try std.fs.cwd().openFile(file_path, .{});
     defer file.close();
+
     var delimiter = [_]u8{'\n'};
-    var buf_reader = std.io.bufferedReader(file.reader());
-
     var line_count: u64 = 0;
-    var buffer: [1024 * 3]u8 = undefined;
-
-    line_count = try read_file(&buf_reader, &buffer, &delimiter);
+    var buf_reader = std.io.bufferedReader(file.reader());
+    std.debug.print("buffer len: {d}bytes\n", .{buffer.len});
+    line_count = try read_file(&buf_reader, buffer, &delimiter);
     std.debug.print("Line count: {d}\n", .{line_count});
 }
 
+// Reads the file by buffer length and return its line count.
+// Returns the line count (u64)
 pub fn read_file(buffer_reader: anytype, buffer: []u8, delimiter: []u8) anyerror!u64 {
     var number_of_lines: u64 = 0;
     while (true) {
